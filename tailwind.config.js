6ef32d7a3e5d5d99680fa88a0ad6951272bf7d91
Sanderson Vieira <sandersonvieira26@gmail.com>
2026-05-29 07:08:51 -0300

    style: configurar Tailwind com identidade visual Pantera Roxa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pantera: {
+          purple: '#AA00FF',
+          pink: '#E040FB',
+          dark: '#7B1FA2',
+          darker: '#4A0060',
+          black: '#0D0010',
+          lavender: '#CE93D8',
+          card: '#1a0025',
+        },
+        income: '#1D9E75',
+        expense: '#E24B4A',
+        pending: '#BA7517',
+      },
+      fontFamily: {
+        display: ['"Bebas Neue"', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
